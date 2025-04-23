da613492b24b4d5bda9a0b8ef7d6b96693b597a1
AbrarHossain666 <abrarhossainifrad@gmail.com>
2025-04-23 22:13:58 +0200

    update

--- script.js
+++ script.js
@@ -15,3 +15,4 @@ darkMode.addEventListener('click', function() {
     darkMode.classList.toggle('active'); // Optional: Toggle 'active' class for styling the button (if needed)
     content.classList.toggle('night');  // Toggle the 'night' class on the body for dark mode
 });
+
